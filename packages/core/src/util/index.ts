60cfb77b2e04ae5c16d873592ee161faa0c075f3
Jason Kaharudin <jasonakaharudin@gmail.com>
2024-08-11 18:38:42 +0800

    CRUD notes

---
+++ packages/core/src/util/index.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/prefer-namespace-keyword */
+import { Context, APIGatewayProxyEvent } from "aws-lambda";
+
+export namespace Util {
+  export function handler(
+    lambda: (evt: APIGatewayProxyEvent, context: Context) => Promise<string>
+  ) {
+    return async function(event: APIGatewayProxyEvent, context: Context) {
+      let body: string, statusCode: number;
+
+      try {
+        // Run the Lambda
+        body = await lambda(event, context);
+        statusCode = 200;
+      } catch (error) {
+        statusCode = 500;
+        body = JSON.stringify({
+          error: error instanceof Error ? error.message : String(error),
+        });
+      }
+
+      // Return HTTP response
+      return {
+        body,
+        statusCode,
+      };
+    };
+  }
+}
